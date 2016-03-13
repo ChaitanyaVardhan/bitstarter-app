c450c859e42ea326e19503a691476bee96ac2714
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2016-03-13 23:28:48 +0000

    modified constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,8 @@
 var Constants = {
     APP_NAME: "Bitstarter",
     PRODUCT_NAME: "STOCK PHOTO APP",
-    PRODUCT_SHORT_DESCRIPTION: "stock photo app with micropayments"
+    PRODUCT_SHORT_DESCRIPTION: "stock photo app with micropayments",
+    COINBASE_PREORDER_DATA_CODE: "13b56883764b54e6ab56fef3bcc7229c"
 }
 
 module.exports = Constants;
