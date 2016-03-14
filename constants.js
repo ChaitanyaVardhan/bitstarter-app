691ffbd142550d20197f729f3371ad03fcc2a31d
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2016-03-14 20:06:21 +0000

    modified constants.js

--- constants.js
+++ constants.js
@@ -2,6 +2,8 @@ var Constants = {
     APP_NAME: "Bitstarter",
     PRODUCT_NAME: "STOCK PHOTO APP",
     PRODUCT_SHORT_DESCRIPTION: "stock photo app with micropayments",
+    TWITTER_USERNAME: "nodejs",
+    TWITTER_TWEET: "This student crowdfunder looks interesting.",
     COINBASE_PREORDER_DATA_CODE: "13b56883764b54e6ab56fef3bcc7229c"
 }
 
